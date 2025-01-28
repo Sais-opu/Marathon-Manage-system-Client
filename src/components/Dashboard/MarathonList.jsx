6ba2c3029ec9faec46e8fea3893a4c1d99a68c7b
Sais-opu <saidulislamapu2024@gmail.com>
2025-01-28 09:24:51 +0600

    marathonlist stil

--- src/components/Dashboard/MarathonList.jsx
+++ src/components/Dashboard/MarathonList.jsx
@@ -1,9 +1,48 @@
-import React from 'react';
+import { useEffect, useState } from "react";
+import { useContext } from "react";
+import { AuthContext } from "../Provider/authProvider";
+
+
+
 
 const MarathonList = () => {
+    const [mylist, setMyList] = useState([])
+
+    const { user } = useContext(AuthContext);
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/job-applylist?email=${user.email}`)
+            .then(res => res.json())
+            .then(data => setMyList(data))
+    }, [user.email])
+
+
     return (
         <div>
-            <h2>Hello</h2>
+            <h2>Hello{mylist.length}</h2>
+            <div className="overflow-x-auto">
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Marathon Title</th>
+                            <th>Regitration Deadline</th>
+                            <th>Favorite Color</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            mylist.map((list,index)=><tr key={list._id}>
+                                <th>{index+1}</th>
+                                <td>{list.Title}</td>
+                                <td>{list.EndRegistrationDate }</td>
+                                <td>Blue</td>
+                            </tr>)
+                        } 
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
